db423e6e451e0cd3148474cb696aab5714ade89d
Harsh Srivastava <srivastavah240@gmail.com>
2024-11-20 23:25:40 +0530

    (feat):Working with Fourth grid in Landing Page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -8,10 +8,26 @@ import MockDiscordUI from "@/components/mockDiscordUi"
 import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
 import DiscordMessage from "@/components/discord-message"
 import Image from "next/image"
+import SyntaxHighlighter from "react-syntax-highlighter"
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 type Props = {}
 
 const page = (props: Props) => {
+  const codeSnippet = `await fetch("http://localhost:3000/api/v1/events", {
+    method: "POST",
+    body: JSON.stringify({
+      category: "sale",
+      fields: {
+        plan: "PRO",
+        email: "zoe.martinez2001@email.com",
+        amount: 49.00
+      }
+    }),
+    headers: {
+      Authorization: "Bearer <YOUR_API_KEY>"
+    }
+  })`
   return (
     <>
       <section className="relative py-24  sm:py-32  bg-brand-25">
@@ -241,6 +257,65 @@ const page = (props: Props) => {
 
                </div>
                {/* Fourth Bento Grid Element */}
+
+               <div className=" group relative lg:row-span-2">
+                <div className=" absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"/>
+                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                  <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                    <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Easy Integration 
+
+                    </p>
+                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600  max-lg:text-center">
+                      Connect <span className="font-semibold text-brand-700">PingPanda</span>{" "}
+                      with your existing workflows in minutes and call our intutive logging <span className="font-semibold text-purple-700"> API</span>{" "} from any  language.
+                    </p>
+                  </div>
+                  <div className="relative min-h-[30rem] w-full grow">
+                    <div className="absolute bottom-0 left-10 right-0  top-10 overflow-hidden rounded-tl-xl  bg-gray-900 shadow-2xl">
+                      <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                      <div className="-mb-px flex text-sm/6  font-medium text-gray-400">
+                      <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2  text-white">
+                      Pingpanda.ts
+                      </div>
+
+                      </div>
+                      
+                      </div>
+                      <div className="overflow-hidden">
+                        <div className="max-h-[30rem]">
+                          <SyntaxHighlighter
+                           language="typescript"
+                           style={{...oneDark,
+                            'pre[class*="language-"]':{
+                              ...oneDark['pre[class*="language-"]'],
+                              background:"transparent",
+                              overflow:"hidden",
+                            },
+                            'code[class*="language-"]':
+                            {
+                              ...oneDark['code[class*="language-1"]'],
+                            background:"transparent",
+                          }
+                        }}
+                          >
+                          {codeSnippet}
+                          </SyntaxHighlighter>
+
+                        </div>
+                        
+
+                      </div>
+                    </div>
+
+                  </div>
+                  
+                </div>
+
+                <div className=" pointer-events-none absolute inset-px rounded-lg shadow ring-4  max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]ring-blue-300  group-hover:ring-purple-300" />
+
+               </div>
+               
           </div>
         </MaxWidthWrapper>
       </section>
