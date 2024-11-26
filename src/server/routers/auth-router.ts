d9c1e98c95a02c325a2ba6e13685c37943fccdcf
Harsh Srivastava <srivastavah240@gmail.com>
2024-11-26 23:31:35 +0530

    (feat):added router for synching clerk user with db. Need to work on frontend React Query

--- src/server/routers/auth-router.ts
+++ src/server/routers/auth-router.ts
@@ -1,12 +1,39 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { router } from "../__internals/router";
 import { publicProcedure } from "../procedures";
+import { db } from "@/db";
 
 // auth router  
 export const authRouter=router({
     //GET request to get the status of the database. "/api/auth/getDatabaseSyncStatus"
-    getDatabaseSyncStatus:publicProcedure.query(({c})=>{
+    //ctx => context  that we get from middleware
+    getDatabaseSyncStatus:publicProcedure.query(async({c ,ctx})=>{
+        const auth =await currentUser();
+        if(!auth)
+        {
+            return c.json({
+                isSynced:false
+            })
+        }
+        const user=await db.user.findFirst({
+            where:{externalId:auth.id},
+        })
+        //Sync Clerk  User with Db
+        if(!user)
+        {
+            await db.user.create({
+                data:{
+                    quotaLimit:100,
+                    externalId:auth.id,
+                    email:auth.emailAddresses[0].emailAddress,
+                }
+            })
+            return c.json({
+                isSynced:true
+            })
+        }
         return c.json({
-            status:"success"
+             isSynced:true
         })
     })
 })
