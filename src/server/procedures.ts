9d41dfbe54c62a29d76feed7c1010c69d1f620dc
Harsh Srivastava <srivastavah240@gmail.com>
2024-11-29 17:28:37 +0530

    (feat):added authMiddleware

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,8 +1,41 @@
 
+import { db } from "@/db";
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server";
+import { HTTPException } from "hono/http-exception";
 
-const authMiddleware=j.middleware(({next})=>{
-const user= {name:"Cheeku"}
+const authMiddleware=j.middleware(async({c,next})=>{
+    const authHeader=c.req.header("Authorization");
+    if(authHeader)
+    {
+        //get api key from header   Bearer <API_KEY>
+        const apiKey= authHeader.split(" ")[1];
+        //get user from api key
+        const  user= await db.user.findUnique({
+            where:{apikey:apiKey},
+        })
+        //got user in our db
+        if(user)
+        {
+            return next({user});
+        }
+        
+    }
+    //if no api key in header but  user is signed in using clerk
+    const auth =await currentUser();
+    if(!auth)
+    {
+        throw new HTTPException(401, {message:"Unauthorized user"})
+    }
+    //get user from  db
+    const user =await db.user.findUnique({
+        where:{externalId:auth.id} ,
+    })
+    if(!user)
+    {
+        throw new HTTPException(401, {message:"Unauthorized user"})
+    }
+  
 return  next({user});
 })
 /**
