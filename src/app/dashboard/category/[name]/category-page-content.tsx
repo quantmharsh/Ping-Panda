56a4ffad6cf7a8212a0831127f24344e6e98b560
Harsh Srivastava <srivastavah240@gmail.com>
2025-01-02 23:51:32 +0530

    (feat): Implement pagination and time range filtering for event retrieval in category page

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -2,6 +2,10 @@
 import { EventCategory } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { EmptyCategoryState } from "./empty-category-state"
+import { useSearchParams } from "next/navigation"
+import { useMemo, useState } from "react"
+import { client } from "@/app/lib/client"
+import{ColumnDef} from "@tanstack/react-table"
 
 
 
@@ -16,11 +20,45 @@ export const CategoryPageContent=({
 }:CategoryPageContentProps)=>{
 
 
+  const searchParams= useSearchParams()
+  const[activeTab , setActiveTab]=useState<"today"|"week"|"month">("today");
+  const page=parseInt(searchParams.get("page")||"1" ,10);
+  const limit=parseInt(searchParams.get("limit")||"30" ,10);
+  const[pagination ,setPagination]=useState({
+    pageIndex:page-1,
+    pageSize:limit,
+  })
+
   const {data:pollingData}=useQuery({
     queryKey:["category" , category.name , "hasEvents"],
     initialData:{hasEvents:initialHasEvents},
 
   })
+
+  const {data , isFetching}=useQuery({
+    queryKey:["events", category.name , pagination.pageIndex , pagination.pageSize , activeTab,],
+    queryFn:async()=>{
+      const res =await client.category.getEventByCategoryName.$get({
+        name:category.name ,
+        page:pagination.pageIndex+1,
+        limit:pagination.pageSize,
+        timeRange:activeTab,
+
+      })
+      return await res.json();
+    },
+    refetchOnWindowFocus:false,
+    enabled:pollingData?.hasEvents,
+    
+  })
+  // const columns:ColumnDef<Event>[]=useMemo(
+  //   ()=>[
+     
+
+  //   ]
+  // )
+  
+
 
   if(!pollingData.hasEvents)
   {
