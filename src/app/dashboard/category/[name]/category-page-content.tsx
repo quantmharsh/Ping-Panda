a10e4310343c53991d8cf6f7efad5334922ffaee
Harsh Srivastava <srivastavah240@gmail.com>
2025-01-03 17:23:18 +0530

    (feat): Enhance category page with dynamic event columns and sorting functionality

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -1,11 +1,14 @@
 "use client"
-import { EventCategory } from "@prisma/client"
+import { EventCategory,Event } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { EmptyCategoryState } from "./empty-category-state"
 import { useSearchParams } from "next/navigation"
 import { useMemo, useState } from "react"
 import { client } from "@/app/lib/client"
-import{ColumnDef} from "@tanstack/react-table"
+import{ColumnDef, Row} from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown } from "lucide-react"
+import { cn } from "@/utils"
 
 
 
@@ -51,12 +54,74 @@ export const CategoryPageContent=({
     enabled:pollingData?.hasEvents,
     
   })
-  // const columns:ColumnDef<Event>[]=useMemo(
-  //   ()=>[
-     
+  //since rendering colums  requires huge calculation so storing it in cache using useMemo . Changes only when dependenci array changes. Not update on re-render like useEffect
+  const columns:ColumnDef<Event>[]=useMemo(
+    ()=>[
+      {
+        accessorKey:"category",
+        header:"Category",
+        cell:()=>
+          <span>
+            {category.name ||"Uncategorized"}
+          </span>
+      },
+      {
+        accessorKey:"createdAt",
+        header:({column})=>{
+          console.log("what is column",column);
+          return(
+            <Button
+             variant={"ghost"}
+             onClick={()=>column.toggleSorting(column.getIsSorted()==="asc")
 
-  //   ]
-  // )
+             }
+            >
+              Date <ArrowUpDown className="ml-2 size-4"/>
+            </Button>
+          )
+        },
+        cell:({row})=>{
+          return new Date(row.getValue("createdAt")).toLocaleString()
+
+        },
+      },
+      // remaining fields...
+      ...(data?.events[0] ? Object.keys(data.events[0].fields as object).map((field)=>({
+        accessorFn:(row:Event)=>
+        (row.fields as Record<string ,any>)[field],
+        header:field,
+        cell:({row}: {row:Row<Event>})=>
+        (
+          row.original.fields as Record<string , any>
+        )[field] ||"-"
+
+
+      })):[]),
+
+      {
+        accessorKey:"deliveryStatus",
+        header:"Delivery Status",
+        cell:({row})=>(
+          <span
+          className={cn("px-2 py-1 rounded-full text-xs  font-semibold",{
+            "bg-green-100 text-green-800":
+            row.getValue("deliveryStatus")==="DELIVERED",
+            "bg-red-100 text-red-800":
+            row.getValue("deliveryStatus")==="FAILED",
+            "bg-yellow-100 text-yellow-800":
+            row.getValue("deliveryStatus")==="PENDING",
+            
+          })}
+          >
+            {row.getValue("deliveryStatus")}
+
+
+          </span>
+        ),
+      },
+
+    ],[category.name , data?.events]
+  )
   
 
 
