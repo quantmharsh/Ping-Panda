a6fc028273f4c6f2b8a05e52a077c3e7f67ab2d0
Harsh Srivastava <srivastavah240@gmail.com>
2025-01-15 20:57:56 +0530

    (fix):remove runtime edge

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,5 @@
 import { httpHandler } from "@/server"
-
-export const runtime = "edge"
+//need to remove edge No Money no honey. Uses more then 1 MB which is limit for vercel
+// export const runtime = "edge"
 
 export { httpHandler as GET, httpHandler as POST }
